b9bc24a072bd9c128bdde3ab733848b5b18eac08
Marko Heyns <marko.h@imake.pro>
2019-10-18 01:38:12 +1300

    Added valueChange notification event

--- HGCircularSlider/Classes/CircularSlider.swift
+++ HGCircularSlider/Classes/CircularSlider.swift
@@ -143,36 +143,6 @@ open class CircularSlider: UIControl {
     }
     
     /**
-     * The minimum value of the receiver.
-     *
-     * If you change the value of this property, and the end value of the receiver is below the new minimum, the end point value is adjusted to match the new minimum value automatically.
-     * The default value of this property is 0.0.
-     */
-//    @IBInspectable
-//    open var minimumValue: CGFloat = 0.0 {
-//        didSet {
-//            if endPointValue < minimumValue {
-//                endPointValue = minimumValue
-//            }
-//        }
-//    }
-    
-    /**
-     * The maximum value of the receiver.
-     *
-     * If you change the value of this property, and the end value of the receiver is above the new maximum, the end value is adjusted to match the new maximum value automatically.
-     * The default value of this property is 1.0.
-     */
-//    @IBInspectable
-//    open var maximumValue: CGFloat = 1.0 {
-//        didSet {
-//            if endPointValue > maximumValue {
-//                endPointValue = maximumValue
-//            }
-//        }
-//    }
-
-    /**
     * The offset of the thumb centre from the circle.
     *
     * You can use this to move the thumb inside or outside the circle of the slider
@@ -194,30 +164,6 @@ open class CircularSlider: UIControl {
     open var stopThumbAtMinMax: Bool = false
 
 
-    /**
-     * The value of the endThumb (changed when the user change the position of the end thumb)
-     *
-     * If you try to set a value that is above the maximum value, the property automatically resets to the maximum value.
-     * And if you try to set a value that is below the minimum value, the property automatically resets  to the minimum value.
-     *
-     * The default value of this property is 0.5
-     */
-//    open var endPointValue: CGFloat = 0.5 {
-//        didSet {
-//            if oldValue == endPointValue {
-//                return
-//            }
-//            if endPointValue > maximumValue {
-//                endPointValue = maximumValue
-//            }
-//            else if endPointValue < minimumValue {
-//                endPointValue = minimumValue
-//            }
-//
-//            setNeedsDisplay()
-//        }
-//    }
-    
     /**
      * The radius of circle
      */
@@ -250,6 +196,7 @@ open class CircularSlider: UIControl {
     override public init(frame: CGRect) {
         super.init(frame: frame)
         setup()
+//        NotificationCenter.default.addObserver(self, selector: #selector(self.methodOfReceivedNotification(notification:)), name: Notification.Name("valueChange"), object: nil)
     }
     
     /**
@@ -258,6 +205,7 @@ open class CircularSlider: UIControl {
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
+//        NotificationCenter.default.addObserver(self, selector: #selector(self.methodOfReceivedNotification(notification:)), name: Notification.Name("valueChange"), object: nil)
     }
     
     internal func setup() {
@@ -265,55 +213,6 @@ open class CircularSlider: UIControl {
     }
 
 
-    // MARK: Drawing methods
-    
-    /**
-     See superclass documentation
-     */
-//    override open func draw(_ rect: CGRect) {
-//        guard let context = UIGraphicsGetCurrentContext() else { return }
-//
-//        drawCircularSlider(inContext: context)
-//
-//        let valuesInterval = Interval(min: minimumValue, max: maximumValue, rounds: numberOfRounds)
-//        // get end angle from end value
-//        let endAngle = CircularSliderHelper.scaleToAngle(value: endPointValue, inInterval: valuesInterval) + CircularSliderHelper.circleInitialAngle
-//
-//        drawFilledArc(fromAngle: CircularSliderHelper.circleInitialAngle, toAngle: endAngle, inContext: context)
-//
-//        // draw end thumb
-//        endThumbTintColor.setFill()
-//        (isHighlighted == true) ? endThumbStrokeHighlightedColor.setStroke() : endThumbStrokeColor.setStroke()
-//
-//        drawThumbAt(endAngle, with: endThumbImage, inContext: context)
-//    }
-//
-    // MARK: User interaction methods
-    
-    /**
-     See superclass documentation
-     */
-//    override open func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
-//        sendActions(for: .editingDidBegin)
-//
-//        return true
-//    }
-    
-    /**
-     See superclass documentation
-     */
-//    override open func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
-//        // the position of the pan gesture
-//        let touchPosition = touch.location(in: self)
-//        let startPoint = CGPoint(x: bounds.center.x, y: 0)
-//        let value = newValue(from: endPointValue, touch: touchPosition, start: startPoint)
-//
-//        endPointValue = value
-//        sendActions(for: .valueChanged)
-//
-//        return true
-//    }
-    
     /**
      See superclass documentation
      */
@@ -466,8 +365,16 @@ open class CircularSlider: UIControl {
             }
             
             setNeedsDisplay()
+            
+            NotificationCenter.default.post(name: Notification.Name("valueChange"), object: nil, userInfo: ["startPointChanged":"\(startPointValue)"])
         }
     }
+    
+    
+    
+//    @objc func methodOfReceivedNotification(notification: Notification) {
+//        print("methodOfReceivedNotification \(notification.userInfo)")
+//    }
     
     /**
      * The value in the end thumb.
@@ -493,6 +400,8 @@ open class CircularSlider: UIControl {
             }
             
             setNeedsDisplay()
+
+            NotificationCenter.default.post(name: Notification.Name("valueChange"), object: nil, userInfo: ["endPointChanged":"\(startPointValue)"])
         }
     }
     
@@ -609,7 +518,6 @@ open class CircularSlider: UIControl {
             endPointValue = value
         }
         sendActions(for: .valueChanged)
-        
         return true
     }
 
